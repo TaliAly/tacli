aa9424c4c51a648f3f5754fe936b5be602bff52b
Tali <allysonnomuraga@gmail.com>
2024-07-15 02:36:55 -0500

    bubbling terminal

--- main.ts
+++ main.ts
@@ -1,4 +1,31 @@
-export default function greet() {
-  console.log('hey')
+import { execSync } from 'child_process'
+import readline from 'node:readline'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+function main() {
+  prompt()
 }
-greet()
+main()
+
+function prompt() {
+  const promise = new Promise((res) => {
+    rl.question('\n> ', (answer) => {
+      res(answer)
+    })
+  })
+
+  promise.then((res) => {
+    if (res == 'quit') {
+      rl.close()
+      return
+    }
+    execSync(String(res), {
+      stdio: [0, 1, 2],
+    })
+    prompt()
+  })
+}
