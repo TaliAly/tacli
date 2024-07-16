d44dc3264168b324abffb11ba54576a8a1812808
Tali <allysonnomuraga@gmail.com>
2024-07-15 21:43:01 -0500

    adding commander for CLI

--- main.ts
+++ main.ts
@@ -1,31 +1,41 @@
-import { execSync } from 'child_process'
+import { execSync, spawnSync } from 'child_process'
+import { Command } from 'commander'
 import readline from 'node:readline'
+import { argv } from 'process'
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
+const program = new Command()
+program
+  .version('1.0.0')
+  .description('a funny warp-like copycat')
+  .option('-a, --ask', 'ask the AI about something', ask)
 
-function main() {
-  prompt()
-}
-main()
+program.command('ask').description('ask the AI for help').action(ask)
 
-function prompt() {
-  const promise = new Promise((res) => {
-    rl.question('\n> ', (answer) => {
-      res(answer)
-    })
+program.parse(argv)
+
+function ask() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   })
+  async function prompt() {
+    const promise = new Promise((res) => {
+      rl.question('\n> ', (answer) => {
+        res(answer)
+      })
+    })
 
-  promise.then((res) => {
-    if (res == 'quit') {
+    const res = await promise
+
+    if (res == 'quit' || res == 'exit') {
       rl.close()
       return
     }
-    execSync(String(res), {
+    const process = execSync(String(res), {
       stdio: [0, 1, 2],
     })
+
     prompt()
-  })
+  }
+  prompt()
 }
